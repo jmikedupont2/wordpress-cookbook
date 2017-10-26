a3d8218a3a1e62c1ffed1614ce7359b62369c516
Matt Mencel <mr-mencel@wiu.edu>
2017-10-26 16:36:24 -0500

    fix grant

--- recipes/database.rb
+++ recipes/database.rb
@@ -101,6 +101,7 @@ if is_local_host? db['host']
   mysql_database_user db['user'] do
     connection    mysql_connection_info
     database_name db['name']
+    password      dbsecure['pass']    
     privileges    [:all]
     action        :grant
   end
